6808a1666dd622dc6f0e82795bd925e43ce28f20
Patricio Ferreira <patricio.ferreira@build.com>
2019-09-09 22:44:25 -0700

    migration: refactoring and building the skeleton.

---
+++ lib/archetypes/default/README.md
@@ -0,0 +1,20 @@
+## Synapse Package Configuration
+
+<h4>Codebase</h4>
+* Typescript
+
+<h4>Backend</h4>
+* ExpressJs
+* Nunjucks
+
+<h4>Bundling</h4>
+* Rollup
+
+<h4>Front End</h4>
+* SystemJS - SystemJs-hmr
+* React
+* Redux
+* Tachyons
+
+<h4>Unit Testing</h4>
+* Jest
---
+++ test/integration/synapse.config.js
@@ -0,0 +1,73 @@
+module.exports = {
+	"source": "./src",
+	"target": "./public",
+	"system": "./js/system.config.js",
+	"server": {
+		"public": "./public",
+		"rewrites": [
+			{
+				"source": ":url",
+				"destination": "html/:url.html"
+			}
+		],
+		"cleanUrls": true,
+		"port": 3000
+	},
+	"plugins": [
+		{
+			"name": "includepaths",
+			"options": {
+				"include": {
+					"systemjs": "./node_modules/systemjs/dist/s.js"
+				}
+			}
+		},
+		{
+			"name": "node-resolve",
+			"options": {
+				"module": true,
+				"jsnext": true,
+				"main": true,
+				"browser": true
+			}
+		},
+		{
+			"name": "commonjs",
+			"options": {
+				"include": "node_modules/**"
+			}
+		}
+	],
+	"html": {
+		"path": "./html",
+		"srcExtension": ".nunjucks",
+		"outExtension": ".html"
+	},
+	"test": {
+		"libs": [
+			"js/libraries/libraries-test.ts",
+			"js/libraries/libraries-dev.ts",
+			"js/libraries/libraries-prod.ts"
+		],
+		"output": {
+			"format": "system"
+		}
+	},
+	"dev": {
+		"libs": [
+			"js/libraries/libraries-dev.ts",
+			"js/libraries/libraries-prod.ts"
+		],
+		"output": {
+			"format": "system"
+		}
+	},
+	"prod": {
+		"libs": [
+			"js/libraries/libraries-prod.ts"
+		],
+		"output": {
+			"format": "system"
+		}
+	}
+};
