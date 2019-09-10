b80bcc02b69077afb430de535852d72c381c64b8
Patricio Ferreira <patricio.ferreira@build.com>
2019-09-09 18:48:40 -0700

    migration: Fixe babel 7 env for development. oclif commands load phase fixed.

---
+++ @types/index.d.ts
@@ -0,0 +1,4 @@
+/**
+ *  TODO Typings for Synapse
+ *  @author Patricio Ferreira <3dimentionar@gmail.com>
+ **/
--- release.js
+++ release.js
@@ -2,4 +2,5 @@
  * Release Script
  * @author Patricio Ferreira <3dimentionar@gmail.com>
  */
+require('@babel/register');
 // TODO: Create Release Script
