58db741f8dd77196387c32ecd34cbc03c41805f9
Patricio Ferreira <patricio.ferreira@build.com>
2020-02-19 19:51:01 -0800

    migration: Added extend as dependency. Improve Command options injection via decorators. Progress on utils.adt.Collection class.

---
+++ test/unit/commands/init.spec.js
@@ -0,0 +1,29 @@
+/**
+ * SynapseCommand Class Specs
+ * @module common
+ * @author Patricio Ferreira <3dimentionar@gmail.com>
+ */
+import { InitCommand } from 'commands/init';
+
+describe('class InitCommand', function() {
+	describe('constructor()', () => {
+		it('should instantiate class', () => {
+			this.command = new InitCommand();
+			assert.instanceOf(this.command, InitCommand);
+		});
+	});
+
+	describe('methods', () => {
+		beforeEach(() => {
+			this.mockCommand = sandbox.mock(this.command);
+		});
+
+		afterEach(() => {
+			sandbox.restore();
+		});
+
+		describe('getTasks()', () => {
+			it('should retrieve tasks', () => {});
+		});
+	});
+});
