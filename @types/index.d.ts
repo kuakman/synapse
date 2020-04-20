7b774d862917fb9368b3a8663fc53dfe40225dde
Patricio Ferreira <patricio.ferreira@build.com>
2020-04-19 17:36:37 -0700

    migration: more progress.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -9,6 +9,98 @@ import { Prompt } from 'enquirer';
  * @namespace synapse
  */
 declare namespace synapse {
+	/** Synapse Configuration **/
+
+	export type SynapseScript = string | string[] | undefined | null;
+
+	export interface SynapseScripts {
+		// Executed once through the command `synapse init` or `synapse update`
+		'create'?: SynapseScript;
+
+		// Pipeline Execution Start
+		'pre-start'?: SynapseScript;
+
+		// Pipeline Execution for Configuration
+		'pre-config'?: SynapseScript;
+		'config'?: SynapseScript;
+		'post-config'?: SynapseScript;
+
+		// Pipeline Execution Clean (Optional)
+		'pre-clean'?: SynapseScript;
+		'clean'?: SynapseScript;
+		'post-clean'?: SynapseScript;
+
+		// Pipeline Execution before environment
+		'pre-env'?: SynapseScript;
+
+		// Pipeline Execution Test Environment
+		'pre-test'?: SynapseScript;
+		'test'?: SynapseScript;
+		'post-test'?: SynapseScript;
+
+		// Pipeline Execution Development Environment
+		'pre-dev'?: SynapseScript;
+		'dev'?: SynapseScript;
+		'post-dev'?: SynapseScript;
+
+		// Pipeline Execution Production Environment
+		'pre-prod'?: SynapseScript;
+		'prod'?: SynapseScript;
+		'post-prod'?: SynapseScript;
+
+		// Pipeline Execution after environment
+		'post-env'?: SynapseScript;
+
+		// Pipeline Execution serve phase
+		'pre-serve'?: SynapseScript;
+		'serve'?: SynapseScript;
+		'post-serve'?: SynapseScript;
+
+		// Pipeline Execution for Release phase
+		'pre-release'?: SynapseScript;
+		'release'?: SynapseScript;
+		'post-release'?: SynapseScript;
+
+		// Pipeline Execution End
+		'pre-end'?: SynapseScript;
+	}
+
+	export interface SynapseEnvironment {
+		[index: string]: object;
+		backend?: object;
+		frontend?: object;
+	}
+
+	export interface SynapseEnvironments {
+		test?: SynapseEnvironment;
+		dev?: SynapseEnvironment;
+		prod: SynapseEnvironment;
+	}
+
+	export interface SynapseRealtime {
+		server: object;
+		auth: { // FIXME: Security SSH-based
+			username: string;
+		};
+		update: 'auto' | 'manual';
+	}
+
+	export interface SynapseConfiguration {
+		source: string;
+		target: string;
+		scripts?: SynapseScripts;
+		environments?: SynapseEnvironments;
+		realtime?: SynapseRealtime;
+	}
+
+	export type Synapse = {
+		synapse: SynapseConfiguration;
+		devDependencies?: { [index: string]: string };
+		dependencies?: { [index: string]: string };
+	}
+
+	/** END SynapseConfiguration **/
+
 	/**
 	 * @interface ErrorOptions
 	 */
@@ -41,6 +133,7 @@ declare namespace synapse {
 		answer(answer: object): ScaffoldCommand;
 		createPrompt(Prompt: FunctionConstructor, question?: object): Promise<Prompt>|null;
 		prompt(question?: object): Promise<object|object[]>|null;
+		read(): Promise<ScaffoldCommand>;
 		write(): Promise<ScaffoldCommand>;
 		install(): Promise<ScaffoldCommand>;
 	}
