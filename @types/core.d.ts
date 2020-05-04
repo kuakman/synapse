5378d2fcb7f8bc48c555095eb72bae0edeaceafc
Patricio Ferreira <patricio.ferreira@build.com>
2020-05-04 13:00:42 -0700

    migration: more progress refactoring. Unit tests still failing...

--- @types/core.d.ts
+++ @types/core.d.ts
@@ -1,10 +1,12 @@
 /**
- * Core Archetype Typings
+ * Synapse Core Typings
  * @author Patricio Ferreira <3dimentionar@gmail.com>
  * @module synapse-core
  */
 declare module 'synapse-core' {
 	// Imports
+	import EventEmitter from 'events';
+	import { StdioOptions } from 'child_process';
 	import { Collection } from 'synapse-utils';
 
 	/**
@@ -60,50 +62,93 @@ declare module 'synapse-core' {
 	export type EnvironmentType = 'all' | 'test' | 'dev' | 'prod';
 
 	/**
-	 * @interface Environment
-	 * // TODO
+	 * @interface Core
+	 * @extends EventEmitter
 	 */
-	export interface Environment {
-		backend?: any;
-		frontend?: any;
+	export interface Core extends EventEmitter {
+		readonly defaults: Partial<Core>;
+
+		attachEvents(): Core;
+		execute(methodName: string, ...args: any[]): any;
+		parse(attributes?: Partial<Core>): Core;
+		serialize(instance: any, ...omit: string[]): Partial<Core>;
+	}
+
+	/**
+	 * @interface Environment
+	 * @extends Core
+	 */
+	export interface Environment extends Core {
+		readonly name: EnvironmentType | undefined;
+		readonly config: object | undefined;
+
+		parse(attributes?: Partial<Environment>): Environment;
+		parseName(attributes?: Partial<Environment>): Environment;
+		parseConfig(attributes?: Partial<Environment>, environment?: Environment): Environment;
+	}
+
+	/**
+	 * @interface Script
+	 * @extends Core
+	 */
+	export interface Script extends Core {
+		readonly name: ScriptType | undefined;
+		readonly queue: Collection<string>;
+		readonly stdio: StdioOptions;
+
+		isSupported(name: string): boolean;
+		parse(attributes?: Partial<Script>): Script;
+		parseName(attributes?: Partial<Script>): Script;
+		parseQueue(attributes?: Partial<Script>, script?: Script): Script;
+
+		run(): Promise<any>;
+		create(scriptPath: string): Promise<Promise<any>>;
+		execute(prevResponse: any, scriptPath: string, resolve: Function, reject: Function): Promise<void>;
 	}
 
 	/**
 	 * @interface Synapse
+	 * @extends Core
 	 */
-	export interface Synapse {
-		source: string;
-		target: string;
-		scripts?: { [script: keyof ScriptType]: string | string[] };
-		environments?: { [environment: keyof EnvironmentType]: Environment };
-	}
+	export interface Synapse extends Core {
+		readonly source: string;
+		readonly target: string;
+		readonly scripts?: Collection<Script>;
+		readonly environments?: Collection<Environment>;
 
-	/**
-	 * @interface Configuration
-	 */
-	export interface Configuration {
-		[index: string]: any;
-		[dependency: keyof DependenciesType]: string[];
-		synapse: Synapse;
+		parse(attributes?: Partial<Synapse>): Synapse;
+		parseScripts(attributes?: Partial<Synapse>): Synapse;
+		parseEnvironments(attributes?: Partial<Synapse>): Synapse;
 	}
 
 	/**
 	 * @interface Archetype
-	 * @extends NodeJS.EventEmitter
+	 * @extends Core
 	 */
-	export interface Archetype extends NodeJS.EventEmitter {
+	export interface Archetype extends Core {
 		readonly uri: string | undefined;
 		readonly type: string | undefined;
 		readonly name: string | undefined;
 		readonly defaults: Partial<Archetype>;
 		readonly extend: Collection<string>;
 		readonly synapse: Synapse;
-		readonly dependencies: Collection<string>;
-		readonly devDependencies: Collection<string>;
-		readonly peerDependencies: Collection<string>;
+		readonly dependencies: Collection<DependenciesType>;
+		readonly devDependencies: Collection<DependenciesType>;
+		readonly peerDependencies: Collection<DependenciesType>;
 
-		load(): Archetype;
-		parse(): object;
-		toJSON(): object;
+		resolveUri(): string;
+		load(): Promise<Archetype>;
+		parse(attributes?: Partial<Archetype>): Archetype;
+		parseExtend(attributes?: Partial<Archetype>): Archetype;
+		parseSynapse(attributes?: Partial<Archetype>): Archetype;
+		parseDependencies(attributes?: Partial<Archetype>): Archetype;
+		parseDevDependencies(attributes?: Partial<Archetype>): Archetype;
+		parsePeerDependencies(attributes?: Partial<Archetype>): Archetype;
+		toJSON(...omit: string[]): Partial<Archetype>;
 	}
+
+	/**
+	 * @type Configuration
+	 */
+	export type Configuration = Pick<Archetype, 'synapse' | 'dependencies' | 'devDependencies' | 'peerDependencies'>;
 }
